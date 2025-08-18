f291de69046fb48f72cc29dfb8cf34046c32d3f5
Jimmy Okoth <133122498+themanbuildingpipelines@users.noreply.github.com>
2025-08-18 12:12:02 -0700

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,1 +1,92 @@
+--CREATING DDL Scripts for Silver Tables
 
+--check if table crm_cust_info exists, if it exists, drop and create a new table
+IF OBJECT_ID('Silver.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE Silver.crm_cust_info
+GO
+CREATE TABLE Silver.crm_cust_info(
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+--check if table crm_prd_info exists, if it exists, drop and create new table
+IF OBJECT_ID('Silver.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE Silver.crm_prd_info
+GO
+  
+CREATE TABLE Silver.crm_prd_info (
+	prd_id INT,
+	cat_id NVARCHAR(50),
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+  
+--check if crm_sales_details exists, if it exists, drop and create new table
+IF OBJECT_ID('Silver.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE Silver.crm_sales_details
+GO
+  
+CREATE TABLE Silver.crm_sales_details (
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cus_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+--check if erp_cust_az12 exists, if it exists, drop and create new table
+IF OBJECT_ID('Silver.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE Silver.erp_cust_az12
+GO
+  
+CREATE TABLE Silver.erp_cust_az12(
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+  
+--check if erp_loc_a101, if it exists, drop and create new table
+IF OBJECT_ID('Silver.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE Silver.erp_loc_a101
+GO
+
+CREATE TABLE Silver.erp_loc_a101(
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+--check if erp_px_cat_g1v2 exists, if it exists, drop and create new table
+IF OBJECT_ID('Silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE Silver.erp_px_cat_g1v2
+GO
+  
+CREATE TABLE Silver.erp_px_cat_g1v2(
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
